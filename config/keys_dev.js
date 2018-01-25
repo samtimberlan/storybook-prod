d24482743c032647b44393bdcf9c2b5c28716be0
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2018-01-25 21:38:38 +0100

    Add references

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:'mongodb://CHANGEME',
-  googleClientID:'CHANGEME',
-  googleClientSecret:'CHANGEME'
+  mongoURI:'mongodb://samtimberlan:helloWorld@ds249757.mlab.com:49757/tim-storybook-dev',
+  googleClientID:'612186306534-uv4u12snai4l9cc8869gnb8bnqevea4b.apps.googleusercontent.com',
+  googleClientSecret:'lmRIxgsLs4qd5BiC5aIRk_mf'
 }
