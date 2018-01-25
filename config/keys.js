151e5c89e0afd6283777627cfb4b81095068d71f
DESKTOP-G6T2AL6\timud <timudoma@gmail.com>
2018-01-25 21:41:41 +0100

    Refractor code

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,11 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
+// if(process.env.NODE_ENV === 'production'){
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+module.exports = {
+  mongoURI:'mongodb://samtimberlan:helloWorld@ds249757.mlab.com:49757/tim-storybook-dev',
+  googleClientID:'612186306534-uv4u12snai4l9cc8869gnb8bnqevea4b.apps.googleusercontent.com',
+  googleClientSecret:'lmRIxgsLs4qd5BiC5aIRk_mf'
 }
